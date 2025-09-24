f002fde2905e35b8cf259983d1a7b1a0c897d547
Yousef Dawood <yousefdawood31@gmail.com>
2025-09-24 22:17:01 +0300

    implement GitHub sign-in functionality with loading state and error handling

--- src/features/auth/providers/GithubLogin.tsx
+++ src/features/auth/providers/GithubLogin.tsx
@@ -1,11 +1,59 @@
+"use client";
+
+import { useTransition } from "react";
 import { FaGithub as GithubLogo } from "react-icons/fa";
+import { LoaderCircle } from "lucide-react";
+import { toast } from "sonner";
+import { useSpinDelay } from "spin-delay";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+async function handleSignIn() {
+  await authClient.signIn.social({
+    provider: "github",
+    callbackURL: "/",
+
+    fetchOptions: {
+      onSuccess: () => {
+        toast.success("You signed in successfully");
+      },
+
+      onError: (error) => {
+        console.log(error.error.message);
+        toast.error("Something went wrong while signing you in");
+      },
+    },
+  });
+}
 
 export default function GithubLogin() {
+  const [isTransitionPending, startTransition] = useTransition();
+  const isPending = useSpinDelay(isTransitionPending, {
+    delay: 200,
+    minDuration: 250,
+  });
+
   return (
-    <Button className="space-x-2.5 text-lg font-semibold" variant={"outline"}>
-      <GithubLogo />
-      <span>Sign in with GitHub</span>
+    <Button
+      className="space-x-2.5 text-lg font-semibold"
+      variant={"outline"}
+      onClick={() => {
+        startTransition(async () => {
+          await handleSignIn();
+        });
+      }}
+    >
+      {isPending ? (
+        <>
+          <LoaderCircle className="animate-spin" />
+          <span>Loading...</span>
+        </>
+      ) : (
+        <>
+          <GithubLogo />
+          <span>Sign in with GitHub</span>
+        </>
+      )}
     </Button>
   );
 }
