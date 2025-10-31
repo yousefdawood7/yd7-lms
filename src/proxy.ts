70eab1f72ec178595395e9363b7efeea03b4751f
Yousef Dawood <yousefdawood31@gmail.com>
2025-10-31 15:49:45 +0200

    fix the proxy's exported function

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const session = await auth.api.getSession({ headers: await headers() });
   // prettier-ignore
   if (session)
