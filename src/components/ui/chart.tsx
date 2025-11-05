a59d56d7486d69946565e5268d8e7a5a75871602
Yousef Dawood <yousefdawood31@gmail.com>
2025-11-05 16:48:44 +0200

    fix some styling issues

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
+
 import { cn } from "@/lib/utils"
 
 // Format: { THEME_NAME: CSS_SELECTOR }
@@ -348,9 +349,9 @@ function getPayloadConfigFromPayload(
 
 export {
   ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
   ChartLegend,
   ChartLegendContent,
   ChartStyle,
-  ChartTooltip,
-  ChartTooltipContent,
 }
